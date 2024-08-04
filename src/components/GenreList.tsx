712226e44e3621cf808c9260f106fd5eac106ba9
kiganaoakuma <daltonhberg@icloud.com>
2024-08-03 22:51:52 -0700

    transfer to mac studio

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,10 +2,10 @@ import { Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <Text key={genre.id}>{genre.name}</Text>
       ))}
     </ul>
